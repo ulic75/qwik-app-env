fdb94364a5d09a4d11b49d7801e4dbaa4a1a75d3
ulic75 <ulic75@gmail.com>
2023-04-21 10:44:39 -0600

    use onGet to log environment variable

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import type { DocumentHead, RequestEvent } from '@builder.io/qwik-city';
 
 import Counter from '~/components/starter/counter/counter';
 import Hero from '~/components/starter/hero/hero';
 import Infobox from '~/components/starter/infobox/infobox';
 import Starter from '~/components/starter/next-steps/next-steps';
+
+export const onGet = (requestEvent: RequestEvent) => {
+  console.log(requestEvent.env.get('DB_PRIVATE_KEY')); // undefined
+};
 
 export default component$(() => {
   return (
